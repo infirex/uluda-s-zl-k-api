6d4fcb0ade184bb1ea36cd7c996bb87ad108efbe
infirex <33436309+infirex@users.noreply.github.com>
2019-04-10 14:52:58 +0300

    Update sozluk.js

--- sozluk.js
+++ sozluk.js
@@ -1,4 +1,3 @@
-//my key erdemahme239282 
 class Sozluk{
     async entryleriGetir(baslik){
         let link=`http://www.uludagsozluk.com/api/?a=erdemahme239282&n=baslik&b=${baslik}&ls=0&le=50&desc=1`;
